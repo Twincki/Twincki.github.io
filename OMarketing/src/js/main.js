4c15e9a72130ca2448655c6ba1549fd200601a69
Twincki <selutin.a@mail.ru>
2022-04-29 22:41:17 +0300

    Свертал 5,6,7 секцию и footer

--- OMarketing/src/js/main.js
+++ OMarketing/src/js/main.js
@@ -1,3 +1,9 @@
 $(function(){
   var mixer = mixitup('.portfolio_content');
+  $('.slider_blog-inner').slick({
+    dots: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="icons/slider_blog/arrow-left.svg" alt="arrow-left"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="icons/slider_blog/arrow-right.svg" alt="arrow-right"></button>'
+  });
+
 });
